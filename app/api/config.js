00baf2ad14ba3935f93b8b6cc6c859e3c03d2379
Vetochka38 <veta.pavlencko@gmail.com>
2024-05-25 12:20:26 +0800

    ссылка

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://pindie-backend-irk.nomoredomainswork.ru'
+export const BASE_URL = 'http://pindie-backend-irk.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
